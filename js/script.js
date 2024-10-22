d369bc20d841f792f99792be8c1517356566e899
nstratiev <n.stratiev@gmail.com>
2024-10-22 20:01:38 +0300

    localStorage functionality added

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ const btnToTop = document.querySelector('#btn-to-top');
 window.addEventListener('scroll', onScreenScroll);
 btnToTop.addEventListener('click', goToScreenTop);
 
+setInitialLocalStorage();
+
+// FUNCTIONS
 // --- Screen scroll functions
 function onScreenScroll() {
   if (
@@ -20,4 +23,12 @@ function onScreenScroll() {
 function goToScreenTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+// Local Storage functions
+
+function setInitialLocalStorage() {
+  localStorage.setItem('sourdoughCalculators', JSON.stringify({}));
+  localStorage.setItem('yeastCalculators', JSON.stringify({}));
+  localStorage.setItem('banicaCalculators', JSON.stringify({}));
 }
